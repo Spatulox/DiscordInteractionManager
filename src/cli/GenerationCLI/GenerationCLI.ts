2548880aa935e94e48e284fd88fcdb51013d651c
Spatulox <M78stormtrooper@laposte.net>
2026-02-11 14:13:00 +0100

    Removes modal generation feature
    This commit removes the modal generation feature.
    
    The modal generator CLI and related files have been removed to simplify the CLI and focus on other more commonly used generation features.

--- src/cli/GenerationCLI/GenerationCLI.ts
+++ src/cli/GenerationCLI/GenerationCLI.ts
@@ -1,6 +1,5 @@
 import {BaseCLI, MenuSelectionCLI} from "../BaseCLI";
 import {ContextMenuGeneratorCLI} from "./ContextMenuGeneratorCLI";
-import {ModalGeneratorCLI} from "./ModalGeneratorCLI";
 import {SlashCommandGeneratorCLI} from "./SlashCommandsGeneratorCLI";
 
 export class GenerationCLI extends BaseCLI {
@@ -12,7 +11,6 @@ export class GenerationCLI extends BaseCLI {
     protected readonly menuSelection: MenuSelectionCLI = [
         { label: 'Generate Slash Command Template', action: () => new SlashCommandGeneratorCLI(this) },
         { label: 'Generate Context Menu Template', action: () => new ContextMenuGeneratorCLI(this) },
-        { label: 'Generate Modal Template', action: () => new ModalGeneratorCLI(this) },
         { label: 'Back', action: () => this.goBack() },
     ];
 
