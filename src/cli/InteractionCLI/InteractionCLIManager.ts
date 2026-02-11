ba10f0c90bd40a52e2247e4188f4263bbe595289
Spatulox <M78stormtrooper@laposte.net>
2026-02-11 16:06:37 +0100

    Enhances command selection and management
    Improves command management by allowing users to select
    commands from both local files and remote servers for
    update, and deletion.
    
    Adds prompts to distinguish between global and guild-specific
    commands during update and delete operations, enhancing user
    control and clarity.

--- src/cli/InteractionCLI/InteractionCLIManager.ts
+++ src/cli/InteractionCLI/InteractionCLIManager.ts
@@ -47,37 +47,64 @@ export class InteractionManagerCLI extends BaseCLI {
     }
 
     private async handleDeploy(): Promise<void> {
-        const selected = await this.selectCommands(this.manager, false);
+        const selected = await this.selectCommands(this.manager, await this.manager.listFromFile());
         if (selected.length === 0) return;
         await this.manager.deploy(selected);
     }
 
     private async handleUpdate(): Promise<void> {
-        const selected = await this.selectCommands(this.manager);
+
+        const rep = await this.yesNoInput("Do you want to update a global command or a specific guild command (y=global/n=specific): ")
+
+        console.log('═'.repeat(80));
+        console.log(`ACTUAL DEPLOYED ${this.manager.folderPath?.toUpperCase()}`)
+        if(rep){
+            await this.listRemote()
+        } else {
+            await this.guildListRemote(await new GuildListManager(Env.clientId, Env.token).chooseGuild());
+        }
+        console.log('═'.repeat(80));
+        console.log(`ACTUAL LOCAL ${this.manager.folderPath?.toUpperCase()}`)
+        const selected = await this.selectCommands(this.manager, await this.manager.listFromFile(false));
         if (selected.length === 0) return;
         await this.manager.update(selected);
     }
 
     private async handleDelete(): Promise<void> {
-        const selected = await this.selectCommands(this.manager);
+
+        const rep = await this.yesNoInput("Do you want to delete a global command or a specific guild command (y=global/n=specific): ")
+
+        let commands: Command[]
+        if(rep){
+            commands = await this.manager.list()
+        } else {
+            const guild = await new GuildListManager(Env.clientId, Env.token).chooseGuild()
+            if(!guild){
+                console.log("Error, cannot find guild")
+                return
+            }
+            commands = await this.manager.listGuild(guild?.id)
+        }
+
+        const selected = await this.selectCommands(this.manager, commands);
         if (selected.length === 0) return;
         await this.manager.delete(selected);
     }
 
-    private async selectCommands(manager: BaseInteractionManager, remote: boolean = true): Promise<Command[]> {
+    private async selectCommands(manager: BaseInteractionManager, commands: Command[]): Promise<Command[]> {
         const handlerManagerType = `${manager.folderPath}(s)`;
-        const commandList = remote ? await manager.list() as any[] : await manager.listFromFile() as any[];
-        if (!commandList?.length) {
+
+        if (!commands?.length) {
             console.log(`No ${handlerManagerType} found`);
             return [];
         }
 
         const input = await this.prompt('Enter numbers (ex: 1,3,5 or "all" or "exit"): ');
-        if (input.toLowerCase() === 'all') return commandList;
+        if (input.toLowerCase() === 'all') return commands;
         if (input.toLowerCase() === 'exit') return [];
 
         const indices = input.split(',').map(i => parseInt(i.trim())).filter(i => !isNaN(i));
-        const selected = commandList.filter((cmd: any) => indices.includes(cmd.index));
+        const selected = commands.filter((cmd: any) => indices.includes(cmd.index));
 
         if (selected.length === 0) {
             console.log('Invalid number');
