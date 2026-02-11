327a1b54178c94dda98b75e2d3f6036f584606a1
Spatulox <M78stormtrooper@laposte.net>
2026-02-11 11:08:35 +0100

    Adds environment variable handling
    Introduces a new module for managing environment variables,
    including validation of the bot client ID using a regular expression.
    
    This ensures required configurations are present and valid before the bot starts,
    improving reliability and preventing runtime errors due to missing or
    incorrect configuration.

---
+++ src/Env.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+import {DiscordRegex} from "./utils/DiscordRegex";
+dotenv.config();
+
+export const Env = {
+    get token(): string {
+        const token = process.env.DISCORD_BOT_TOKEN;
+        if (!token) throw new Error('Missing environment variable : DISCORD_BOT_TOKEN');
+        return token;
+    },
+    get dev(): boolean {
+        return process.env.DEV === 'true';
+    },
+    get clientId(): string {
+        const token = process.env.DISCORD_BOT_CLIENTID;
+        if (!token) throw new Error('Missing environment variable : DISCORD_BOT_CLIENTID');
+
+        if(!DiscordRegex.BOT_ID.test(token)){
+            throw new Error("Invalid token format")
+        }
+
+        return token;
+    }
+} as const;
